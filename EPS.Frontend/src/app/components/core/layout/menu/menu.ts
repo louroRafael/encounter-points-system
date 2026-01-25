fbe2d8558e2dade97f5386e7e4e7445fb5c440d0
louroRafael <rafaryse@outlook.com>
2026-01-25 00:57:05 -0300

    improve content and adjust responsivity

--- EPS.Frontend/src/app/components/core/layout/menu/menu.ts
+++ EPS.Frontend/src/app/components/core/layout/menu/menu.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatIconModule
+  ],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
 export class Menu {
+  isMenuOpen = false;
   menuItems = ['Home', 'O que é', 'Encontre o seu', 'FAQ', 'Contato'];
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
--- EPS.Frontend/src/app/components/core/layout/page/page.ts
+++ EPS.Frontend/src/app/components/core/layout/page/page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Menu } from '../menu/menu';
 
 @Component({
   selector: 'app-page',
-  imports: [Menu],
+  imports: [
+    CommonModule,
+    Menu
+  ],
   templateUrl: './page.html',
   styleUrl: './page.css',
 })
 export class Page {
+  activeBg: string = 'futebol';
 
+  epCategories = [
+    { label: 'Futebol', key: 'futebol' },
+    { label: 'Boardgame', key: 'boardgame' },
+    { label: 'Escalada', key: 'escalada' },
+    { label: 'Bike', key: 'bike' },
+    { label: 'Corrida', key: 'corrida'},
+    { label: 'Trilha', key: 'trilha'}
+  ];
+
+  getBackground(): string {
+    return `url(/assets/images/bg-${this.activeBg}.jpg)`;
+  }
 }
